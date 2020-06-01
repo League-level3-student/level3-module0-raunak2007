8bb080a2d0bd8b37210a2181214ad366f1a89386
raunak2007 <spmondal08@gmail.com>
2020-06-01 16:59:25 -0700

    level 3 module 1
    :smile:

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -5,18 +5,22 @@ import java.util.Random;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-
+		String [] cheat= {"nba","nfl","nhl"};
 		//2. print the third element in the array
-
+		System.out.println(cheat[2]);
 		//3. set the third element to a different value
-
+		cheat[2]="trump";
 		//4. print the third element again
-		
+		System.out.println(cheat[2]);
 		//5. use a for loop to set all the elements in the array to a string of your choice
-		
+		for(int i=0;i<cheat.length;i++) {
+			cheat[i]=i+"";
+		}
 		//6. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for(int i=0;i<cheat.length;i++) {
+			System.out.println(cheat[i]);
+		}
 		//7. make an array of 50 integers
 
 		//8. use a for loop to make every value of the integer array a random number
--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -8,23 +8,48 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
-	
+	public static void main(String[] args) {
+		
 		//2. create an array of 5 robots.
-
+        Robot[] rob=new Robot[5];
+        Robot winner;
+        boolean reached=false;
+        int numWinner=0;
 		//3. use a for loop to initialize the robots.
-
+        for(int i=0;i<5;i++) {
+        	rob[i]=new Robot();
+        	rob[i].setX(100*i+20);
+        	rob[i].setY(500);
+        }
 			//4. make each robot start at the bottom of the screen, side by side, facing up
 	
 		//5. use another for loop to iterate through the array and make each robot move 
 	    //   a random amount less than 50.
-    	
+        while(reached==false) {
+        	for(int j=0;j<5;j++) {
+        		rob[j].setSpeed(15);
+        		int rand=new Random().nextInt(50);
+        		int currentY=rob[j].getY();
+        		rob[j].moveTo(100*j+20,currentY-rand);
+        		if(rob[j].getY()<=10) {
+        			reached=true;
+        			if(numWinner<=0) {
+        				int winnerNum;
+        				winnerNum=j;
+        				winner=rob[j];
+        				JOptionPane.showMessageDialog(null, "Robot "+winnerNum+" won!");
+        			}
+        			numWinner++;
+        		}
+        	}
+        }
 		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 		//7. declare that robot the winner and throw it a party!
-    	
 		//8. try different races with different amounts of robots.
     	
 	    //9. make the robots race around a circular track.
 
 	
+}
 }
